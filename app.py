b97e3f1accf6b443b75ff122c514128fd0bd1f86
LopesSoh <sophiac.lopes2008@gmail.com>
2025-08-19 13:17:43 -0300

    criação da função de cadastro e inicio do loop principal

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
+from classes import*
+from func import*
 
+menu()
+resp1 = int(input("--> "))
+
+while resp1 != 0:
+    if resp1 == 1:
+        limpar_e_pausar()
+        menu_cadastro()
+        tipo = int(input("--> "))
+
+        print("Preencha as informações do animal:")
+        nome = input("Nome: ")
+        peso = input("Peso: ")
+        idade = input("Idade: ")
+        raca = input("Raça ou especie:")
+        cor = input("Cor: ")
+        dono = input("Nome do dono:")
+
+        cadastro(tipo, nome, peso, idade, raca, cor, dono)
+        print("Cadastro realizado com sucesso!")
+    
+    else:
+        print(" ")
