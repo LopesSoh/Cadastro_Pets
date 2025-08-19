6c0875336d85228164ccc8843942e3501ddcaf69
LopesSoh <sophiac.lopes2008@gmail.com>
2025-08-19 10:01:16 -0300

    criação defs de controle (menu  e LeP

---
+++ func.py
@@ -0,0 +1,11 @@
+import os
+
+#função menu
+def menu():
+    print("--MENU-- \n Digite a opção desejada: \n 1-Realizar Cadastro \n 2-Atualizar Cadastro \n 3-Listar Cadastrados")
+
+#função limpar e pausar tela
+def limpar_e_pausar():
+    os.system('pause')
+    os.system('cls')
+    os.system('pause')
